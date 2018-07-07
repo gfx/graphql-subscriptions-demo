49c4a3935dcfa77a67a0fb0bfb8733e507cffa21
FUJI Goro (gfx) <gfuji@cpan.org>
2018-07-07 21:07:30 +0900

    rename: s/GraphqlRubyDemoSchema/AppSchema/

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -7,7 +7,7 @@ class GraphqlController < ApplicationController
       # Query context goes here, for example:
       # current_user: current_user,
     }
-    result = GraphqlRubyDemoSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = AppSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   end
 
--- app/graphql/app_schema.rb
+++ app/graphql/app_schema.rb
@@ -1,4 +1,4 @@
-class GraphqlRubyDemoSchema < GraphQL::Schema
+class AppSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 end
